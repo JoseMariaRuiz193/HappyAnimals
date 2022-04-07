0e834c826ec3271bad2bee7d33728fcfe2db1dd7
xpereez <777elalexito@gmail.com>
2022-04-07 17:08:35 +0200

    Splash+Orientacion landscape

--- app/src/main/java/com/example/happyanimals/MainActivity2.kt
+++ app/src/main/java/com/example/happyanimals/MainActivity2.kt
@@ -1,5 +1,6 @@
 package com.example.happyanimals
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,7 @@ class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        //ORientacion
     }
 }
