709c66eecf254bcc2caa1e1c9a9fc70c887c7694
jose <josealcorcon193@gmail..com>
2022-04-21 17:13:53 +0200

    // Implementado sonido cerdo // Creadas variables para tablet de las pantallas activity main 2 y teoria sonidos

--- app/src/main/java/com/example/happyanimals/TeoriaSonidos.kt
+++ app/src/main/java/com/example/happyanimals/TeoriaSonidos.kt
@@ -1,14 +1,37 @@
 package com.example.happyanimals
 
+import android.content.Context
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.ImageButton
+import android.widget.Toast
+
 
 class TeoriaSonidos : AppCompatActivity() {
+
+
+    private lateinit var cerdoPlayButton: ImageButton
+    private lateinit var mediaPlayer: MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_teoria_sonidos)
+
+        cerdoPlayButton = findViewById(R.id.cerdoPlayButton)
+        mediaPlayer = MediaPlayer.create(this, R.raw.cerdo)
+
+        setOnClickListeners(this)
         supportActionBar?.hide()
         this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+    }
+    private fun setOnClickListeners(context: Context) {
+       cerdoPlayButton.setOnClickListener {
+            mediaPlayer.start()
+            Toast.makeText(context, "Reproduciendo...", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 }
