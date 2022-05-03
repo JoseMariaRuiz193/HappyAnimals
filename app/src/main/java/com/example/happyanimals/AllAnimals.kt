e0628adff4e8fec64b06dc0f7544beb4fd763fa0
jose <josealcorcon193@gmail..com>
2022-05-03 20:51:14 +0200

    // la pantalla allAnimals lleva pinchando el cerdo lleva a la pantalla del cerdo

--- app/src/main/java/com/example/happyanimals/AllAnimals.kt
+++ app/src/main/java/com/example/happyanimals/AllAnimals.kt
@@ -1,7 +1,9 @@
 package com.example.happyanimals
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.WindowManager
 
 class AllAnimals : AppCompatActivity() {
@@ -11,5 +13,13 @@ class AllAnimals : AppCompatActivity() {
         supportActionBar?.hide()
         this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
+    }
+
+    fun onClickTeoriaSonidos(view: View) {
+        irPantallaTeoriaSonidos()
+    }
+    fun irPantallaTeoriaSonidos() {
+        val pantallaTeoriaSonidos = Intent(this, TeoriaSonidos::class.java)
+        startActivity(pantallaTeoriaSonidos)
     }
 }
